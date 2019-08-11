0fc54b8f4194feb02f3da5923920b41af1c82963
Tomislav Markovski <tomislav@streetcred.id>
2019-08-11 18:21:31 -0400

    Added starter configuration

---
+++ WebAgent/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using AgentFramework.Core.Contracts;
+using AgentFramework.Core.Handlers.Agents;
+using AgentFramework.Core.Models.Records;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAgent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IAgentProvider agentProvider;
+        private readonly IProvisioningService provisioningService;
+
+        public HomeController(IAgentProvider agentProvider, IProvisioningService provisioningService)
+        {
+            this.agentProvider = agentProvider;
+            this.provisioningService = provisioningService;
+        }
+
+        // GET api/home
+        [HttpGet]
+        public async Task<ProvisioningRecord> Get()
+        {
+            var agentContext = await agentProvider.GetContextAsync();
+            var record = await provisioningService.GetProvisioningAsync(agentContext.Wallet);
+
+            return record;
+        }
+    }
+}
---
+++ WebAgent/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using AgentFramework.AspNetCore;
+using AgentFramework.Core.Models.Wallets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAgent
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAgentFramework(builder =>
+            {
+                builder.AddBasicAgent(config =>
+                {
+                    config.WalletConfiguration = new WalletConfiguration { Id = "WebAgent" };
+                    config.WalletCredentials = new WalletCredentials { Key = "SecretKey" };
+                    config.EndpointUri = new Uri("http://localhost:5000");
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseAgentFramework();
+            app.UseMvc();
+        }
+    }
+}
